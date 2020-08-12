2771c1cf94e3462e9320468926f19cff4563b4ca
Ryan Greenup <exogenesis@protonmail.com>
2020-08-12 16:22:51 +1000

    Started on Research Outline

---
+++ Outline/.auctex-auto/Research-Outline.el
@@ -0,0 +1,48 @@
+(TeX-add-style-hook
+ "Research-Outline"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "11pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem") ("biblatex" "citestyle=numeric" "bibstyle=numeric" "hyperref=true" "backref=true" "maxcitenames=3" "url=true" "backend=biber" "natbib=true")))
+   (add-to-list 'LaTeX-verbatim-environments-local "minted")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art11"
+    "inputenc"
+    "fontenc"
+    "graphicx"
+    "grffile"
+    "longtable"
+    "wrapfig"
+    "rotating"
+    "ulem"
+    "amsmath"
+    "textcomp"
+    "amssymb"
+    "capt-of"
+    "hyperref"
+    "minted"
+    "biblatex")
+   (LaTeX-add-labels
+    "sec:orgce8eada"
+    "sec:org587bcb4"
+    "sec:org84dfa04"
+    "sec:orgd98c738"
+    "sec:org9fe3ac9"
+    "sec:org60dd603"
+    "sec:org849fcff"
+    "sec:orgdaa04a3")
+   (LaTeX-add-bibliographies
+    "../Resources/references"))
+ :latex)
+
