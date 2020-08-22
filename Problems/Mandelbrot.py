5cb6bbade4ac6b0480f20b1094e83dba26b4e4fb
Ryan Greenup <exogenesis@protonmail.com>
2020-08-22 14:01:41 +1000

    Sketched out Mandelbrot Plot

---
+++ Problems/Mandelbrot.py
@@ -0,0 +1,65 @@
+from math import sqrt
+def magnitude(z):
+    # return sqrt(z[0]**2 + z[1]**2)
+    x = z[0]
+    y = z[1]
+    return sqrt(sum(map(lambda x: x**2, [x, y])))
+
+def cAdd(a, b):
+    x = a[0] + b[0]
+    y = a[1] + b[1]
+    return [x, y]
+
+
+def cMult(u, v):
+    x = u[0]*v[0]-u[1]*v[1]
+    y = u[1]*v[0]+u[0]*v[1]
+    return [x, y]
+    
+
+def mandelbrot(z, num):
+    ''' runs the process num amount of times and returns the count of
+    divergence'''
+    count = 0
+    # Define z1 as z
+    z1 = z
+    # Iterate num times
+    while count <= num:
+        # check for divergence
+        if magnitude(z1) > 2.0:
+            #return the step it diverged on
+            return count
+        #iterate z
+        z1 = cAdd(cMult(z1, z1),z)
+        count+=1
+        #if z hasn't diverged by the end
+    return num
+
+import numpy as np
+
+
+p = 0.1 #horizontal, vertical, pinch (zoom)
+res = 2000
+h = res/2
+v = res/2
+
+pic = np.zeros([res, res])
+for i in range(pic.shape[0]):
+    for j in range(pic.shape[1]):
+        x = (j - h)/(p*res)
+        y = (i-v)/(p*res)
+        z = [x, y]
+        col = mandelbrot(z, 100)
+        pic[i, j] = col
+
+pic
+
+        
+import matplotlib.pyplot as plt
+plt.imshow(pic)
+# plt.show()
+
+import plotly.express as px
+fig = px.imshow(pic)
+fig.show()
+
