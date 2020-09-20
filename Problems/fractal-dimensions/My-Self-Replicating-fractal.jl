f62d37f0b5f011815e92a78ed678faf3a6fca6dc
Ryan Greenup <exogenesis@protonmail.com>
2020-09-20 12:15:43 +1000

    Better Plot of My Fractal

--- Problems/fractal-dimensions/My-Self-Replicating-fractal.jl
+++ Problems/fractal-dimensions/My-Self-Replicating-fractal.jl
@@ -17,17 +17,39 @@ function mywalk(B, n)
     end
     return B
 end
+############################################################
+#### Render Image ##########################################
+#################yellow and purple##########################
 
 using GR
-GR.imshow(mywalk([1 1 1 ; 1 0 0 ; 1 0 0], 5))
+GR.imshow(mywalk([1 1], 5))
 
-vals = size(mywalk([1 1 1 ; 1 0 0 ; 1 0 0], 12))
-vals[1]/vals[2]
+############################################################
+##### Use Plot for themes ##################################
+############################################################
+
+using Plots, clibraries
+# Choose a backend, plots let's me choose themes
+gr() # set the backend to GR, faster, but, no lines
+pyplot() # Set the backend to pyplot
+plt = Plots.plot(mywalk([1 1], 6),
+                 st=:heatmap, clim=(0,1),
+                 color=:coolwarm,
+                colorbar_title="", ticks = true, legend = false, yflip = true, fmt = :svg);
+# yflip!
+Plots.savefig(plt, "./my-self-rep-frac.svg")
+;convert ./my-self-rep-frac.svg ./my-self-rep-frac.png
+plt
+
+PyPlot.plot(mywalk([1 1], 7), st=:heatmap, clim=(0,1), color=:coolwarm, colorbar_title="");
 
 #------------------------------------------------------------
 #-- Dimension -----------------------------------------------
 #------------------------------------------------------------
-# Each time it iterates there are 5 more
+# Each time it iterates the image scales by phi
+# and the number of pixels increases by 2
+# so log(2)/log(1.618)
+# lim(F_n/F_n-1)
 # but the overall dimensions of the square increases by a factor of 3
 # so 3^D=5 ==> log_3(5) = log(5)/log(3) = D 
 
@@ -38,6 +60,5 @@ mat1 = mywalk(fill(1, 1, 1), 11)
 l1   = sum(mat1)
 size1 = size(mat1)[1]
 log(l2/l1)/log(size2/size1)
-# https://en.wikipedia.org/wiki/Vicsek_fractal#Construction
-# log(5)/log(3) # what would this dimension be ???
+log(2)/log(1.618) # what would this dimension be ???
 
