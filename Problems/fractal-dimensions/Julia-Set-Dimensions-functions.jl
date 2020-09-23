341857b16d9694176e09021fd0fc1acca3e0a9d9
Ryan Greenup <exogenesis@protonmail.com>
2020-09-23 21:12:22 +1000

    Seperated Julia Function into two seperate files, functions and interaction

---
+++ Problems/fractal-dimensions/Julia-Set-Dimensions-functions.jl
@@ -0,0 +1,155 @@
+using GR
+using DataFrames
+using Gadfly
+using GLM
+using SharedArrays
+using Distributed
+
+############################################################
+### Julia / MandelBrot Functions ###########################
+############################################################
+
+"""
+# Julia Set
+Returns how many iterations it takes for a value on the complex plane to diverge
+under recursion. if `boolQ` is specified as true a 1/0 will be returned to
+indicate divergence or convergence.
+
+## Variables
+- `z`
+  - A value on the complex plane within the unit circle
+- `num`
+  - A number of iterations to perform before conceding that the value is not
+    divergent.
+- `my_func`
+  - A function to perform on `z`, for a julia set the function will be of the
+    form `z -> z^2 + a + im*b`
+    - So for example the Douady Rabbit would be described by `z -> z^2 -0.123+0.745*im` 
+"""
+function juliaSet(z, num, my_func, boolQ=true)
+    count = 1
+    # Define z1 as z
+    z1 = z
+    # Iterate num times
+    while count ≤ num
+        # check for divergence
+        if real(z1)^2+imag(z1)^2 > 2^2
+            if(boolQ) return 0 else return Int(count) end
+        end
+        #iterate z
+        z1 = my_func(z1) # + z
+        count=count+1
+    end
+        #if z hasn't diverged by the end
+    if(boolQ) return 1 else return Int(count) end
+end
+
+
+"""
+# Mandelbrot Set
+Returns how many iterations it takes for a value on the complex plane to diverge
+under recursion of \$z \\rightarrow z^2 + z_0\$.
+
+Values that converge represent constants of the julia set that lead to a
+connected set. (TODO: Have I got that Vice Versa?)
+
+
+## Variables
+- `z`
+  - A value on the complex plane within the unit circle
+- `num`
+  - A number of iterations to perform before conceding that the value is not
+    divergent.
+- `boolQ`
+  - `true` or `false` value indicating whether or not to return 1/0 values
+    indicating divergence or convergence respecitvely or to return the number of
+   iterations performed before conceding no divergence.
+"""
+function mandelbrot(z, num, boolQ = true)
+    count = 1
+    # Define z1 as z
+    z1 = z
+    # Iterate num times
+    while count ≤ num
+        # check for divergence
+        if real(z1)^2+imag(z1)^2 > 2^2
+            if(boolQ) return 0 else return Int(count) end
+        end
+        #iterate z
+        z1 = z1^2 + z
+        count=count+1
+    end
+        #if z hasn't diverged by the end
+    return 1 # Int(num)
+    if(boolQ) return 1 else return Int(count) end
+end
+
+function test(x, y)
+    if(x<1) return x else return y end
+end
+
+
+############################################################
+##### Build a Matrix Image #################################
+############################################################
+
+"""
+# Make a Picture
+
+This maps a function on the complex plane to a matrix where each element of the
+matrix corresponds to a single value on the complex plane. The matrix can be
+interpreted as a greyscale image.
+
+Inside the function is a `zoom` parameter that can be modified for different
+fractals, fur the julia and mandelbrot sets this shouldn't need to be adjusted.
+
+The height and width should be interpreted as resolution of the image.
+
+- `width`
+  - width of the output matrix
+- `height`
+  - height of the output matrix
+- `myfunc`
+  - Complex Function to apply across the complex plane
+"""
+function make_picture(width, height, my_func)
+    pic_mat = zeros(width, height)
+    zoom = 0.3
+    for j in 1:size(pic_mat)[2]
+        for i in 1:size(pic_mat)[1]
+            x = (j-width/2)/(width*zoom)
+            y = (i-height/2)/(height*zoom)
+            pic_mat[i,j] = juliaSet(x+y*im, 256, my_func)
+        end
+    end
+    return pic_mat
+end
+
+############################################################
+### Make the Outline ########################################
+############################################################
+# TODO this should be inside a function
+
+"""
+# Outline 
+
+Sets all elements with neighbours on all sides to 0.
+
+- `mat`
+  - A matrix
+    - If this matrix is the convergent values corresponding to a julia set the
+      output will be the outline, which is the definition of the julia set.
+"""
+function outline(mat)
+    work_mat = copy(mat)
+    for col in 2:(size(mat)[2]-1)
+        for row in 2:(size(mat)[1]-1)
+            ## Make the inside 0, we only want the outline
+            neighbourhood = mat[row-1:row+1,col-1:col+1]
+            if sum(neighbourhood) >= 9 # 9 squares
+                work_mat[row,col] = 0
+            end
+        end
+    end
+    return work_mat
+end
--- Problems/fractal-dimensions/julia-set-dimensions.jl
+++ Problems/fractal-dimensions/julia-set-dimensions.jl
@@ -1,154 +1,8 @@
-using GR
-############################################################
-### Julia / MandelBrot Functions ###########################
-############################################################
-
-"""
-# Julia Set
-Returns how many iterations it takes for a value on the complex plane to diverge
-under recursion. if `boolQ` is specified as true a 1/0 will be returned to
-indicate divergence or convergence.
-
-## Variables
-- `z`
-  - A value on the complex plane within the unit circle
-- `num`
-  - A number of iterations to perform before conceding that the value is not
-    divergent.
-- `my_func`
-  - A function to perform on `z`, for a julia set the function will be of the
-    form `z -> z^2 + a + im*b`
-    - So for example the Douady Rabbit would be described by `z -> z^2 -0.123+0.745*im` 
-"""
-function juliaSet(z, num, my_func, boolQ=true)
-    count = 1
-    # Define z1 as z
-    z1 = z
-    # Iterate num times
-    while count ≤ num
-        # check for divergence
-        if real(z1)^2+imag(z1)^2 > 2^2
-            if(boolQ) return 0 else return Int(count) end
-        end
-        #iterate z
-        z1 = my_func(z1) # + z
-        count=count+1
-    end
-        #if z hasn't diverged by the end
-    if(boolQ) return 0 else return Int(count) end
-end
-
-
-"""
-# Mandelbrot Set
-Returns how many iterations it takes for a value on the complex plane to diverge
-under recursion of \$z \\rightarrow z^2 + z_0\$.
-
-Values that converge represent constants of the julia set that lead to a
-connected set. (TODO: Have I got that Vice Versa?)
-
-
-## Variables
-- `z`
-  - A value on the complex plane within the unit circle
-- `num`
-  - A number of iterations to perform before conceding that the value is not
-    divergent.
-- `boolQ`
-  - `true` or `false` value indicating whether or not to return 1/0 values
-    indicating divergence or convergence respecitvely or to return the number of
-   iterations performed before conceding no divergence.
-"""
-function mandelbrot(z, num, boolQ = true)
-    count = 1
-    # Define z1 as z
-    z1 = z
-    # Iterate num times
-    while count ≤ num
-        # check for divergence
-        if real(z1)^2+imag(z1)^2 > 2^2
-            if(boolQ) return 0 else return Int(count) end
-        end
-        #iterate z
-        z1 = z1^2 + z
-        count=count+1
-    end
-        #if z hasn't diverged by the end
-    return 1 # Int(num)
-    if(boolQ) return 0 else return Int(count) end
-end
-
-function test(x, y)
-    if(x<1) return x else return y end
-end
-
+@time include("./Julia-Set-Dimensions-functions.jl")
 
 ############################################################
-##### Build a Matrix Image #################################
+#### Investigate Plot #######################################
 ############################################################
-
-"""
-# Make a Picture
-
-This maps a function on the complex plane to a matrix where each element of the
-matrix corresponds to a single value on the complex plane. The matrix can be
-interpreted as a greyscale image.
-
-Inside the function is a `zoom` parameter that can be modified for different
-fractals, fur the julia and mandelbrot sets this shouldn't need to be adjusted.
-
-The height and width should be interpreted as resolution of the image.
-
-- `width`
-  - width of the output matrix
-- `height`
-  - height of the output matrix
-- `myfunc`
-  - Complex Function to apply across the complex plane
-"""
-function make_picture(width, height, my_func)
-    pic_mat = zeros(width, height)
-    zoom = 0.3
-    for j in 1:size(pic_mat)[2]
-        for i in 1:size(pic_mat)[1]
-            x = (j-width/2)/(width*zoom)
-            y = (i-height/2)/(height*zoom)
-            pic_mat[i,j] = juliaSet(x+y*im, 256, my_func)
-        end
-    end
-    return pic_mat
-end
-
-############################################################
-### Make the Outline ########################################
-############################################################
-# TODO this should be inside a function
-
-"""
-# Outline 
-
-Sets all elements with neighbours on all sides to 0.
-
-- `mat`
-  - A matrix
-    - If this matrix is the convergent values corresponding to a julia set the
-      output will be the outline, which is the definition of the julia set.
-"""
-function outline(mat)
-    work_mat = copy(mat)
-    for col in 2:(size(mat)[2]-1)
-        for row in 2:(size(mat)[1]-1)
-            ## Make the inside 0, we only want the outline
-            neighbourhood = mat[row-1:row+1,col-1:col+1]
-            if sum(neighbourhood) >= 9 # 9 squares
-                work_mat[row,col] = 0
-            end
-        end
-    end
-    return work_mat
-end
-
-using GR
 test_mat = make_picture(800,800, z -> z^2 + 0.37-0.2*im)
 test_mat = make_picture(800,800, z -> z^2 + -0.123+0.745*im)
 GR.imshow(test_mat) # PyPlot uses interpolation = "None"
@@ -159,6 +13,7 @@ GR.imshow(test_mat) # PyPlot uses interpolation = "None"
 
 ## Return the perimeter
 sum(test_mat)
+
 
 
 mat2 = outline(make_picture(350,350, z -> z^2 + -0.123+0.745*im))
@@ -184,12 +39,6 @@ log(l2/l1)/log(size2/size1)
   # GR.imshow(make_picture(500, 500, z -> z^2 + 0.38-0.2*im)) # PyPlot uses interpolation = "None"
   # GR.imshow(make_picture(500, 500, z -> z^2 + 0.39-0.2*im)) # PyPlot uses interpolation = "None"
 
-using DataFrames
-using Gadfly
-using GLM
-
-using SharedArrays
-using Distributed
 
 
 function scaleAndMeasure(min, max, n)
