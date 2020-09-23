80d09aa11d3a38322622b01c0ebb3281fb088aa3
Ryan Greenup <exogenesis@protonmail.com>
2020-09-23 20:52:58 +1000

    Added Docstrings to Julia Dimensions

--- Problems/fractal-dimensions/julia-set-dimensions.jl
+++ Problems/fractal-dimensions/julia-set-dimensions.jl
@@ -3,7 +3,23 @@ using GR
 ### Julia / MandelBrot Functions ###########################
 ############################################################
 
+"""
+# Julia Set
+Returns how many iterations it takes for a value on the complex plane to diverge
+under recursion. if `boolQ` is specified as true a 1/0 will be returned to
+indicate divergence or convergence.
 
+## Variables
+- `z`
+  - A value on the complex plane within the unit circle
+- `num`
+  - A number of iterations to perform before conceding that the value is not
+    divergent.
+- `my_func`
+  - A function to perform on `z`, for a julia set the function will be of the
+    form `z -> z^2 + a + im*b`
+    - So for example the Douady Rabbit would be described by `z -> z^2 -0.123+0.745*im` 
+"""
 function juliaSet(z, num, my_func, boolQ=true)
     count = 1
     # Define z1 as z
@@ -22,7 +38,28 @@ function juliaSet(z, num, my_func, boolQ=true)
     if(boolQ) return 0 else return Int(count) end
 end
 
-function mandelbrot(z, num, my_func, boolQ = true)
+
+"""
+# Mandelbrot Set
+Returns how many iterations it takes for a value on the complex plane to diverge
+under recursion of \$z \\rightarrow z^2 + z_0\$.
+
+Values that converge represent constants of the julia set that lead to a
+connected set. (TODO: Have I got that Vice Versa?)
+
+
+## Variables
+- `z`
+  - A value on the complex plane within the unit circle
+- `num`
+  - A number of iterations to perform before conceding that the value is not
+    divergent.
+- `boolQ`
+  - `true` or `false` value indicating whether or not to return 1/0 values
+    indicating divergence or convergence respecitvely or to return the number of
+   iterations performed before conceding no divergence.
+"""
+function mandelbrot(z, num, boolQ = true)
     count = 1
     # Define z1 as z
     z1 = z
@@ -33,7 +70,7 @@ function mandelbrot(z, num, my_func, boolQ = true)
             if(boolQ) return 0 else return Int(count) end
         end
         #iterate z
-        z1 = my_func(z1) 
+        z1 = z1^2 + z
         count=count+1
     end
         #if z hasn't diverged by the end
@@ -44,13 +81,31 @@ end
 function test(x, y)
     if(x<1) return x else return y end
 end
-test(9, 999)
 
 
 ############################################################
 ##### Build a Matrix Image #################################
 ############################################################
 
+"""
+# Make a Picture
+
+This maps a function on the complex plane to a matrix where each element of the
+matrix corresponds to a single value on the complex plane. The matrix can be
+interpreted as a greyscale image.
+
+Inside the function is a `zoom` parameter that can be modified for different
+fractals, fur the julia and mandelbrot sets this shouldn't need to be adjusted.
+
+The height and width should be interpreted as resolution of the image.
+
+- `width`
+  - width of the output matrix
+- `height`
+  - height of the output matrix
+- `myfunc`
+  - Complex Function to apply across the complex plane
+"""
 function make_picture(width, height, my_func)
     pic_mat = zeros(width, height)
     zoom = 0.3
@@ -64,10 +119,6 @@ function make_picture(width, height, my_func)
     return pic_mat
 end
 
-# function make_picture(width, height, my_func)
-#     [ juliaSet((x-width)/2+(y-height)/2*im, 256, my_func ) for x in (1:width)/width, y in (1:height)/height ]
-# end
-
 ############################################################
 ### Make the Outline ########################################
 ############################################################
@@ -75,10 +126,13 @@ end
 
 """
 # Outline 
-Sets all elements with neighbours on all sides to 0
-## TODO
-I should probably set that arbitrary 100 to something related to a mean value
-maybe?
+
+Sets all elements with neighbours on all sides to 0.
+
+- `mat`
+  - A matrix
+    - If this matrix is the convergent values corresponding to a julia set the
+      output will be the outline, which is the definition of the julia set.
 """
 function outline(mat)
     work_mat = copy(mat)
