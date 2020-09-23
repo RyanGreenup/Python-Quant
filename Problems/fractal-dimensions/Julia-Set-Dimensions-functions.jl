aeaf6d180a93478a4a45725ce85006b5f6bab543
Ryan Greenup <exogenesis@protonmail.com>
2020-09-23 23:08:33 +1000

    Reverted back to pmap because the issue was with multi-core generally not pmap/for

--- Problems/fractal-dimensions/Julia-Set-Dimensions-functions.jl
+++ Problems/fractal-dimensions/Julia-Set-Dimensions-functions.jl
@@ -167,13 +167,7 @@ function scaleAndMeasure(min, max, n)
     # #TODO: Prove this
 
     scale = [Int(ceil(i)) for i in range(min, max, length=n) ]
-
-    # pmap kept failing, so for loop it is
-    mass = SharedArray{Float64}(n)
-    @distributed (+) for i = 1:n
-        j = scale[i]
-        mass[i] = sum(outline(make_picture(Int(j), Int(j), z -> z^2 + -0.123+0.745*im)))
-    end
+    mass = pmap(s -> sum(outline(make_picture(Int(s), Int(s), z -> z^2 + -0.123+0.745*im))) , scale)
 
     data = DataFrame(scale = scale, mass = mass)
     return data
