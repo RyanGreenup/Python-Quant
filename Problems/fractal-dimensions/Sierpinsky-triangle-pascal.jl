7f60a015c44845126a8ce966f1f2a0a78dffe819
James-Guerra <64623892+James-Guerra@users.noreply.github.com>
2020-10-01 15:08:57 +1000

    Added a some paragraphs to report

--- Problems/fractal-dimensions/Sierpinsky-triangle-pascal.jl
+++ Problems/fractal-dimensions/Sierpinsky-triangle-pascal.jl
@@ -3,7 +3,7 @@
 # ┃╋┣━┓┃━╋━┳━┓┏┓
 # ┃┏┫╋┗╋━┃━┫╋┗┫┗┓
 # ┗┛┗━━┻━┻━┻━━┻━┛
-
+using GR
 [binomial(j,i) for i in 0:4, j in 0:4]
 [binomial(i,j) for i in 0:4, j in 0:4]
 [binomial(j+i,i) for i in 0:4, j in 0:4]
@@ -20,13 +20,13 @@ GR.savefig("../../Report/media/pascal-sierpinsky-triangle.png")
 #-- Calculate Dimension -------------------------------------
 #------------------------------------------------------------
 
-mat2 = pascal(3000)
-l2   = sum(mat2)
-size2 = size(mat2)[1]
-mat1 = pascal(2000)
-l1   = sum(mat1)
-size1 = size(mat1)[1]
-log(l2/l1)/log(size2/size1)
-# https://en.wikipedia.org/wiki/Sierpi%C5%84ski_triangle
-log(3)/log(2)
+# mat2 = pascal(3000)
+# l2   = sum(mat2)
+# size2 = size(mat2)[1]
+# mat1 = pascal(2000)
+# l1   = sum(mat1)
+# size1 = size(mat1)[1]
+# log(l2/l1)/log(size2/size1)
+# # https://en.wikipedia.org/wiki/Sierpi%C5%84ski_triangle
+# log(3)/log(2)
 
